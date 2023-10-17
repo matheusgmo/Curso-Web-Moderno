ba747850bb6170f220dad9716887513221f5247c
Matheus Moraes <107554927+matheusgmo@users.noreply.github.com>
2023-10-17 15:54:59 -0300

    17/10

--- react/exercicios/src/componentes/Saudacao.jsx
+++ react/exercicios/src/componentes/Saudacao.jsx
@@ -1,7 +1,19 @@
 import React, {Component} from "react";
 
 export default class Saudacao extends Component{
+
+    setTipo(e){
+        console.log(e.target.value)
+    }
     render(){
-        
+        const {tipo, nome} = this.props
+        return (
+            <div>
+                <h1>{tipo} {nome}!</h1>
+                <hr />
+                <input type="text" name="Tipo..." value={tipo} />
+                <input type="text" name="Nome..." value={nome} />
+            </div>
+        )
     }
 }
